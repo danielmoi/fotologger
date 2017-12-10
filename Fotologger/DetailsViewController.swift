2f0ca7107aa21bb466e0bbdbb54c0ff2d5e2e1c8
danielmoi <daniel@ampersandmoi.com>
2017-12-10 11:42:43 +1100

    Add outlets and actions for details view

---
+++ Fotologger/DetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailsViewController.swift
+//  Fotologger
+//
+//  Created by Daniel Moi on 10/12/17.
+//  Copyright © 2017 Daniel Moi. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    @IBOutlet weak var itemImageView: UIImageView!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+
+}
